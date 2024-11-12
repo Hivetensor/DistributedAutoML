9ecb02288d63b56724c7b7e1a0d08a45452dc42d
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2024-11-12 20:50:00 +0300

    add code comments

--- auto_update_miner.py
+++ auto_update_miner.py
@@ -15,6 +15,14 @@ logging.basicConfig(
 
 
 def run_git(cmd: list[str]) -> Optional[str]:
+    """Run a git command and return its output.
+
+    Args:
+        cmd (list[str]): The git command to run.
+
+    Returns:
+        Optional[str]: The output of the git command, or None if the command fails.
+    """
     try:
         return subprocess.run(
             ["git", "-C", REPO_PATH] + cmd, capture_output=True, text=True, check=True
@@ -25,11 +33,25 @@ def run_git(cmd: list[str]) -> Optional[str]:
 
 
 def get_version(content):
+    """Identify the version from the content of dml/chain/__init__.py
+
+    Args:
+        content (str): The content of dml/chain/__init__.py.
+
+    Returns:
+        Optional[int]: The version number, or None if not found.
+    """
     match = re.search(r"__spec_version__\s*=\s*['\"]?(\d+)", content)
     return int(match.group(1)) if match else None
 
 
 def get_versions():
+    """Get the local and remote versions of dml/chain/__init__.py.
+
+    Returns:
+        Tuple[Optional[int], Optional[int]]: The local and remote versions, or None if not found.
+    """
+
     try:
         with open(os.path.join(REPO_PATH, "dml/chain/__init__.py")) as f:
             local_ver = get_version(f.read())
@@ -46,10 +68,13 @@ def get_versions():
 
 def main():
     try:
-        old_commit = run_git(["rev-parse", "HEAD"])
+        old_commit = run_git(
+            ["rev-parse", "HEAD"]
+        )  # Save last commit hash incase update fails
 
         local_ver, remote_ver = get_versions()
         if not all([local_ver, remote_ver]):
+            # No versions found
             return 1
 
         logging.info(f"Local version: {local_ver}, Remote version: {remote_ver}")
@@ -57,19 +82,21 @@ def main():
             logging.info("No update needed")
             return 0
 
+        # git pull latest updates
         result = subprocess.run(
             ["git", "-C", REPO_PATH, "pull"], capture_output=True, text=True
         )
 
         if result.returncode != 0 and "local changes" in result.stderr:
-            #TODO: Potential issues regarding config files
+            # TODO: Potential issues regarding config files
             logging.info("Stashing local changes")
-            run_git(["stash"])
+            run_git(["stash"])  # stash local changes if any found during update
             if not run_git(["pull"]):
                 return 1
             run_git(["stash", "pop"])
 
         try:
+            # test new update to ensure it works fine
             subprocess.run(["pip", "install", "-e", "."], check=True)
             subprocess.run([sys.executable, MAIN_SCRIPT_PATH], check=True)
         except subprocess.CalledProcessError:
